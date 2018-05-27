e2e673286d612e9f1ac025a2cebadf4f51cb22c9
ShwarzeWolf <31793871+ShwarzeWolf@users.noreply.github.com>
2018-05-27 18:31:09 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace MyTheQuestion
+{
+    partial class Form1
+    {
+       
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(760, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 89);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "viewCategories";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1092, 674);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "MyTheQuestion";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿/* связываешь с бд
+ * пишешь простые запросы на отображение выборки
+ * связываешь их с методами
+ * реализуешь методы добавления вопроса
+ * реализуешь методы добавления ответа
+ * 
+ * тестишь
+ * рефакторишь
+ * красиво оформляешь 
+ * 
+ * на выходе команда получает три приложения, каждое из которых делает свою часть 
+ * радуем Хлопотова
+ * profit
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace MyTheQuestion
+{
+
+    public partial class Form1 : Form
+    {
+
+        SqlConnection sqlConnection;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            #region addingListBox
+            ListBox listBox1 = new ListBox();
+          
+            listBox1.Size = new System.Drawing.Size(200, 100);
+            listBox1.Location = new System.Drawing.Point(10, 10);
+            this.Controls.Add(listBox1);
+            listBox1.MultiColumn = true;
+            listBox1.SelectionMode = SelectionMode.MultiExtended;
+            #endregion
+
+            #region establishingConnectionWithDatabase
+            string connectionString = @"Data Source=DESKTOP-4FH9G41\SQLEXPRESS;Initial Catalog=Store;Integrated Security=True";
+            sqlConnection = new SqlConnection(connectionString);
+            await sqlConnection.OpenAsync();
+            SqlDataReader sqlReader = null;
+            SqlCommand sqlCommand = new SqlCommand("SELECT * FROM [CATEGORY]", sqlConnection);
+            #endregion
+
+            try
+            {
+                sqlReader = await sqlCommand.ExecuteReaderAsync();
+
+                while (await sqlReader.ReadAsync())
+                {
+                    listBox1.Items.Add(Convert.ToString(sqlReader["categoryName"]));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString());
+            }
+            finally
+            {
+                if (sqlReader != null) sqlReader.Close();
+            }
+        }
+    }    
+}
